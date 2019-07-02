bdc5c10649c8405db268b0569cc034e92e80c216
Error Handle <mazlol0311@gmail.com>
2019-07-02 18:26:12 +0800

    add count func

--- guess_num.py
+++ guess_num.py
@@ -1,9 +1,11 @@
 import random
 answer = random.randint(1, 100)
-
+count = 0
 while True:
 	value = input('input number:')
 	value = int(value)
+	count += 1
+	print('You guessed ', count, 'times')
 	if answer ==  value:
 		print('correct!')
 		break
